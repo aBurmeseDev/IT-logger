9ffb9768c12623b1502b9d777f6bee23fc11feea
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-07-01 18:10:30 -0700

    retrive data from logs component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './Components/layout/SearchBar';
+import Logs from './Components/logs/Logs';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize';
 import './App.css';
@@ -8,7 +11,14 @@ const App = () => {
     // Init Materialize CSS
     M.AutoInit();
   });
-  return <div className='App'>my app</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;

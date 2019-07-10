222135140c25b5f7cd53b3438622fb713e5eb545
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-07-09 17:34:44 -0700

    edit LogModal proptypes

--- src/Components/logs/EditLogModal.js
+++ src/Components/logs/EditLogModal.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setMessage(current.attention);
+      setMessage(current.tech);
+    }
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
@@ -89,4 +100,16 @@ const modalStyle = {
   width: '75%',
   height: '75%'
 };
-export default EditLogModal;
+
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+export default connect(
+  mapStateToProps,
+  { updateLog }
+)(EditLogModal);

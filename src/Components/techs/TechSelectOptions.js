4a3e03af014b6c75afbe62da7c3aac6f836d77d5
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-07-31 20:43:05 -0700

    add tech options

--- src/Components/techs/TechSelectOptions.js
+++ src/Components/techs/TechSelectOptions.js
@@ -9,7 +9,15 @@ const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
     //eslint-disable-next-line
   }, []);
 
-  return <div />;
+  return (
+    !loading &&
+    techs !== null &&
+    techs.map(t => (
+      <option key={t.id} value={`${t.firstName} ${t.lastName}`}>
+        {t.firstName} {t.lastName}
+      </option>
+    ))
+  );
 };
 
 TechSelectOptions.propTypes = {

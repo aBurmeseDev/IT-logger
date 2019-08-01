ed1a70bf485bf1c76288c63ee1c43e3b640bc1fc
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-07-31 20:29:34 -0700

    create techSelectOptions component

---
+++ src/Components/techs/TechSelectOptions.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getTechs } from '../../actions/techActions';
+
+const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
+  return <div />;
+};
+
+TechSelectOptions.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => {
+  tech: state.tech;
+};
+export default connect(
+  mapStateToProps,
+  { getTechs }
+)(TechSelectOptions);

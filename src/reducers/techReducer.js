7f21d5d1f6c1f2cae7c25c3d0c0c73baeb740534
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-07-31 20:22:43 -0700

    complete techAction/techReducer

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,3 +1,11 @@
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR
+} from '../actions/types';
+
 const initialState = {
   techs: null,
   loading: false,
@@ -5,6 +13,17 @@ const initialState = {
 };
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }

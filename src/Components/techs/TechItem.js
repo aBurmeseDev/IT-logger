771db3b5b8879859bb103afd0266f180c13b9ba8
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-08-01 21:02:31 -0700

    connect deleteTech redux

--- src/Components/techs/TechItem.js
+++ src/Components/techs/TechItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { deleteTech } from '../../actions/techActions';
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
   return (
     <li className='collection-item'>
       <div>
-        {tech.firstName} {tech.lastName}
-        <a href='#!' className='secondary-content'>
+        {firstName} {lastName}
+        <a
+          href='#!'
+          className='secondary-content'
+          onClick={() => deleteTech(id)}
+        >
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
@@ -15,7 +21,11 @@ const TechItem = ({ tech }) => {
 };
 
 TechItem.propTypes = {
-  tech: PropTypes.object.isRequired
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired
 };
 
-export default TechItem;
+export default connect(
+  null,
+  { deleteTech }
+)(TechItem);
